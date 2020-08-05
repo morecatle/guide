ab1c46db9215ad3853d90470aebea4dada4a6007
morecatle <wjddudwn2260@naver.com>
2020-08-05 23:11:11 +0900

    **초기 업데이트**

---
+++ app/src/main/java/com/kakao/guide/IntroActivity.java
@@ -0,0 +1,33 @@
+package com.kakao.guide;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+
+public class IntroActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intro_layout);
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), PresentActivity.class); //넘기기.
+                startActivity(intent);
+                finish();
+            }
+        },3000); // 3초 뒤에 Runner 객체 실행.
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
---
+++ app/src/main/java/com/kakao/guide/PresentActivity.java
@@ -0,0 +1,64 @@
+package com.kakao.guide;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import me.relex.circleindicator.CircleIndicator;
+
+public class PresentActivity extends AppCompatActivity {
+    FragmentPagerAdapter adapterViewPager;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.present_layout);
+
+        ViewPager vpPager = (ViewPager) findViewById(R.id.vpPager);
+        adapterViewPager = new MyPagerAdapter(getSupportFragmentManager());
+        vpPager.setAdapter(adapterViewPager);
+
+        CircleIndicator indicator = (CircleIndicator) findViewById(R.id.indicator);
+        indicator.setViewPager(vpPager);
+    }
+
+    public static class MyPagerAdapter extends FragmentPagerAdapter{
+        private static int NUM_ITEMS = 3;
+
+        public MyPagerAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Page " + position;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return FirstFragment.newInstance(0, "Page # 1");
+                case 1:
+                    return SecondFragment.newInstance(1, "Page # 2");
+                case 2:
+                    return ThirdFragment.newInstance(2, "Page # 3");
+                default:
+                    return null;
+            }
+        }
+
+
+    }
+}
